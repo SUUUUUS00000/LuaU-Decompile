d7499452b78bef3e7914f9e26b6f5e1656d63093
SUUUUUS00000 <specialakk69@gmail.com>
2026-05-04 16:30:36 +0300

    Improve error handling in decompile endpoint
    Refactor error handling in decompile endpoint and improve error messages.

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const express = require('express');
 const core = require('./core');
 const app = express();
 app.use(express.json({ limit: '50mb' }));
+
 app.post('/decompile', (req, res) => {
     try {
         let base64script = req.body.script;
-        if (!base64script) {
-            return res.status(400).send("-- error missing script");
-        }
+        if (!base64script) return res.status(400).send("-- error missing script");
+        
         let result = core.process(base64script);
         res.set('content-type', 'text/plain');
         res.send(result);
     } catch (ex) {
-        res.status(500).send("-- error core crash " + ex.message);
+        res.status(500).send("-- [MEGGD Engine Crash]\n-- " + ex.message + "\n-- " + ex.stack.replace(/\n/g, '\n-- '));
     }
 });
+
 let port = process.env.PORT || 10000;
 app.listen(port, '0.0.0.0', () => {});
