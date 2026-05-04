6f87d17c56b61c6fc2bd112b2ae0b42ebae16ecb
SUUUUUS00000 <specialakk69@gmail.com>
2026-05-04 15:59:11 +0300

    Refactor error handling and server start message

--- server.js
+++ server.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const core = require('./core');
-
 const app = express();
 app.use(express.json({ limit: '50mb' }));
-
 app.post('/decompile', (req, res) => {
     try {
         let base64script = req.body.script;
-        
         if (!base64script) {
-            return res.status(400).send("-- error: empty script");
+            return res.status(400).send("-- error missing script");
         }
-
         let result = core.process(base64script);
         res.set('content-type', 'text/plain');
         res.send(result);
     } catch (ex) {
-        res.status(500).send(`-- [MEGGD Server Crash]\n-- Error: ${ex.message}\n-- StackTrace:\n-- ${ex.stack.replace(/\n/g, '\n-- ')}`);
+        res.status(500).send("-- error core crash " + ex.message);
     }
 });
-
 let port = process.env.PORT || 10000;
-app.listen(port, '0.0.0.0', () => {
-    console.log("server started");
-});
+app.listen(port, '0.0.0.0', () => {});
