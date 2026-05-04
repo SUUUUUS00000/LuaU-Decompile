2c44e84ecc455114fcde54fd7eb7e7fad64ba4a3
SUUUUUS00000 <specialakk69@gmail.com>
2026-05-04 16:00:55 +0300

    Enhance bufferreader with length checks in methods

--- reader.js
+++ reader.js
@@ -2,26 +2,26 @@ class bufferreader {
     constructor(buffer) {
         this.buffer = buffer;
         this.offset = 0;
+        this.length = buffer.length;
     }
-
     readbyte() {
+        if (this.offset >= this.length) return 0;
         let val = this.buffer.readUInt8(this.offset);
         this.offset += 1;
         return val;
     }
-
     readint32() {
+        if (this.offset + 4 > this.length) return 0;
         let val = this.buffer.readInt32LE(this.offset);
         this.offset += 4;
         return val;
     }
-
     readstring(len) {
+        if (this.offset + len > this.length) len = this.length - this.offset;
         let str = this.buffer.toString('utf8', this.offset, this.offset + len);
         this.offset += len;
         return str;
     }
-
     readvarint() {
         let result = 0;
         let shift = 0;
@@ -30,9 +30,9 @@ class bufferreader {
             byte = this.readbyte();
             result |= (byte & 127) << shift;
             shift += 7;
+            if (shift > 35) break; 
         } while (byte & 128);
-        return result;
+        return result >>> 0;
     }
 }
-
 module.exports = bufferreader;
