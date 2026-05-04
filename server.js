6b63c5ef90750f7b39c6cd2ca4a723c78d172bd3
SUUUUUS00000 <specialakk69@gmail.com>
2026-05-04 20:08:27 +0300

    Refactor error messages in server.js

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const core = require('./core');
 const app = express();
+
 app.use(express.json({ limit: '50mb' }));
+
 app.post('/decompile', (req, res) => {
     try {
         let base64script = req.body.script;
-        if (!base64script) return res.status(400).send("-- error missing script");
+        if (!base64script) return res.status(400).send("error missing script");
         let result = core.process(base64script);
         res.set('content-type', 'text/plain');
         res.send(result);
     } catch (ex) {
-        let err = ex.stack ? ex.stack.replace(/\n/g, '\n-- ') : ex.message;
-        res.status(500).send("-- [MEGGD Engine Crash]\n-- " + err);
+        let err = ex.stack ? ex.stack.replace(/\n/g, '\n') : ex.message;
+        res.status(500).send("[MEGGD Engine Crash]\n" + err);
     }
 });
+
 process.on('uncaughtException', () => {});
 process.on('unhandledRejection', () => {});
+
 let port = process.env.PORT || 10000;
 app.listen(port, () => {});
