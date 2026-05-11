c45b60b170cfe476e52d6391ce3701b52df40c47
SUUUUUS00000 <specialakk69@gmail.com>
2026-05-11 18:14:32 +0300

    Update structurizer.h

--- structurizer.h
+++ structurizer.h
@@ -41,22 +41,29 @@ struct Expr {
 };
 
 struct VarExpr : Expr { std::string name; };
-struct ConstExpr : Expr { ConstantType type; union { double num; int64_t intval; std::string str; bool boolean; }; };
+struct ConstExpr : Expr {
+    enum class ConstType { NUMBER, INTEGER, STRING, BOOLEAN, NIL };
+    ConstType type;
+    double num = 0;
+    int64_t intval = 0;
+    std::string str;
+    bool boolean = false;
+};
 struct BinaryExpr : Expr { std::string op; std::unique_ptr<Expr> left, right; };
 struct UnaryExpr : Expr { std::string op; std::unique_ptr<Expr> expr; };
 struct CallExpr : Expr { std::unique_ptr<Expr> func; std::vector<std::unique_ptr<Expr>> args; };
 struct IndexExpr : Expr { std::unique_ptr<Expr> table, index; };
 struct MemberExpr : Expr { std::unique_ptr<Expr> table; std::string field; };
 
-struct IfStmt : Stmt { std::unique_ptr<Expr> cond; std::unique_ptr<BlockStmt> thenBody, elseBody; };
-struct WhileStmt : Stmt { std::unique_ptr<Expr> cond; std::unique_ptr<BlockStmt> body; };
-struct RepeatStmt : Stmt { std::unique_ptr<Expr> cond; std::unique_ptr<BlockStmt> body; };
-struct NumericForStmt : Stmt { std::string var; std::unique_ptr<Expr> from, to, step; std::unique_ptr<BlockStmt> body; };
-struct GenericForStmt : Stmt { std::vector<std::string> vars; std::vector<std::unique_ptr<Expr>> generators; std::unique_ptr<BlockStmt> body; };
-struct AssignStmt : Stmt { std::vector<std::string> vars; std::vector<std::unique_ptr<Expr>> values; };
-struct CallStmt : Stmt { std::unique_ptr<CallExpr> call; };
-struct ReturnStmt : Stmt { std::vector<std::unique_ptr<Expr>> values; };
-struct BlockStmt : Stmt { std::vector<std::unique_ptr<Stmt>> stmts; };
+struct IfStmt : Stmt { std::unique_ptr<Expr> cond; std::unique_ptr<BlockStmt> thenBody, elseBody; void accept(StmtVisitor& v) override { v.visit(*this); } };
+struct WhileStmt : Stmt { std::unique_ptr<Expr> cond; std::unique_ptr<BlockStmt> body; void accept(StmtVisitor& v) override { v.visit(*this); } };
+struct RepeatStmt : Stmt { std::unique_ptr<Expr> cond; std::unique_ptr<BlockStmt> body; void accept(StmtVisitor& v) override { v.visit(*this); } };
+struct NumericForStmt : Stmt { std::string var; std::unique_ptr<Expr> from, to, step; std::unique_ptr<BlockStmt> body; void accept(StmtVisitor& v) override { v.visit(*this); } };
+struct GenericForStmt : Stmt { std::vector<std::string> vars; std::vector<std::unique_ptr<Expr>> generators; std::unique_ptr<BlockStmt> body; void accept(StmtVisitor& v) override { v.visit(*this); } };
+struct AssignStmt : Stmt { std::vector<std::string> vars; std::vector<std::unique_ptr<Expr>> values; void accept(StmtVisitor& v) override { v.visit(*this); } };
+struct CallStmt : Stmt { std::unique_ptr<CallExpr> call; void accept(StmtVisitor& v) override { v.visit(*this); } };
+struct ReturnStmt : Stmt { std::vector<std::unique_ptr<Expr>> values; void accept(StmtVisitor& v) override { v.visit(*this); } };
+struct BlockStmt : Stmt { std::vector<std::unique_ptr<Stmt>> stmts; void accept(StmtVisitor& v) override { v.visit(*this); } };
 
 class Structurizer {
 public:
